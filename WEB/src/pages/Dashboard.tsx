2a53814059cc635393f9d87bbe112f778cd51fee
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-03-04 11:31:00 -0300

    Projeto finalizado da ROCKETSEAT

--- WEB/src/pages/Dashboard.tsx
+++ WEB/src/pages/Dashboard.tsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { api } from "../services/api";
+import { AxiosError } from "axios";
+
 import searchSvg from "../assets/search.svg";
 import { CATEGORIES } from "../utils/categories";
 import { formatCurrency } from "../utils/formatCurrency";
@@ -8,24 +12,46 @@ import { Button } from "../components/Button";
 import { Pagination } from "../components/Pagination";
 import { RefundItem, RefundItemProps } from "../components/RefundItem";
 
-const REFUND_EXAMPLE = {
-  id: "123",
-  name: "Ricardo",
-  category: "transporte",
-  amount: formatCurrency(34.5),
-  categoryImg: CATEGORIES["transport"].icon,
-};
+const PER_PAGE = 5;
 
 export function Dashboard() {
   const [name, setName] = useState("");
   const [page, setPage] = useState(1);
-  const [totalOfPage, setTotalOfPage] = useState(10);
-  const [refund, setRefunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE]);
+  const [totalOfPage, setTotalOfPage] = useState(0);
+  const [refund, setRefunds] = useState<RefundItemProps[]>([]);
 
-  function fetchRefunds(e: React.FormEvent) {
-    e.preventDefault();
+  async function fetchRefunds() {
+    try {
+      const response = await api.get<RefundsPaginationAPIResponse>(
+        `/refunds?name=${name.trim()}&page=${page}&perPage=${PER_PAGE}`
+      );
 
-    console.log(name);
+      setRefunds(
+        response.data.refunds.map((refund) => ({
+          id: refund.id,
+          name: refund.user.name,
+          description: refund.name,
+          amount: formatCurrency(refund.amount),
+          categoryImg: CATEGORIES[refund.category].icon
+        }))
+      )
+
+      setTotalOfPage(response.data.pagination.totalPages)
+      
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      alert("Não foi possível carregar");
+    }
+  }
+
+  function onSubmit(e: React.FormEvent){
+    e.preventDefault()
+    fetchRefunds()
   }
 
   function handlePagination(action: "next" | "previous") {
@@ -42,12 +68,16 @@ export function Dashboard() {
     });
   }
 
+  useEffect(() => {
+    fetchRefunds();
+  }, []);
+
   return (
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
       <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
 
       <form
-        onSubmit={fetchRefunds}
+        onSubmit={onSubmit}
         className="flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6"
       >
         <Input
--- WEB/src/pages/Refund.tsx
+++ WEB/src/pages/Refund.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { z, ZodError } from 'zod'
 import { AxiosError } from "axios";
@@ -10,6 +10,7 @@ import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
 import { Button } from "../components/Button";
+import { formatCurrency } from "../utils/formatCurrency";
 
 const refundSchema = z.object({
   name: z.string().min(3, { message: 'Informe um nome claro para sua solicitação' }),
@@ -25,6 +26,7 @@ export function Refund() {
   const [category, setCategory] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [file, setFile] = useState<File | null>(null);
+  const [fileURL, setFileURL] = useState<string | null>(null)
 
   const navigate = useNavigate();
   const params = useParams<{ id: string }>();
@@ -76,6 +78,32 @@ export function Refund() {
     }
   }
 
+  async function fetchRefund(id: string) {
+    try {
+      const { data } = await api.get<RefundAPIResponse>(`/refunds/${id}`)
+      
+      setName(data.name)
+      setCategory(data.category)
+      setAmount(formatCurrency(data.amount))
+      setFileURL(data.filename)
+
+    } catch (error) {
+      console.log(error)
+
+      if(error instanceof AxiosError){
+        return alert(error.response?.data.message)
+      }
+
+      alert('Não foi possível carregar')
+    }
+  }
+
+  useEffect(() => {
+    if(params.id){
+      fetchRefund(params.id)
+    }
+  },[params.id])
+
   return (
     <form
       onSubmit={onSubmit}
@@ -125,9 +153,9 @@ export function Refund() {
         />
       </div>
 
-      {params.id ? (
+      {params.id && fileURL ? (
         <a
-          href="https://app.rocketseat.com.br/"
+          href={`http://localhost:3333/uploads/${fileURL}`}
           target="_blank"
           className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 transition ease-linear"
         >
